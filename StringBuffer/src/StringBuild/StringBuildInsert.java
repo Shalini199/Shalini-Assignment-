671141a78bbb0543059312f303767b87a87ea81e
shalinpa <shalinpa@LIN25008045.corp.capgemini.com>
2021-11-19 08:46:44 +0530

    String Builder

---
+++ StringBuffer/src/StringBuild/StringBuildInsert.java
@@ -0,0 +1,11 @@
+package StringBuild;
+
+public class StringBuildInsert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuilder str = new StringBuilder("It is used to at the specified index position");
+System.out.println(str.insert(13, " Insert text"));
+	}
+
+}
---
+++ StringBuffer/src/StringBuild/StringBuildReverse.java
@@ -0,0 +1,11 @@
+package StringBuild;
+
+public class StringBuildReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuilder str = new StringBuilder("This method returns the reversed object on which it was called");
+System.out.println(str.reverse());
+	}
+
+}
