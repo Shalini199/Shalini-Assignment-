a429640743c1cb17f05661cd19296aaa8a3a1467
shalinpa <shalinpa@LIN25008045.corp.capgemini.com>
2021-11-19 08:25:26 +0530

    String Assignment

---
+++ String/src/String/StringConcat.java
@@ -0,0 +1,12 @@
+package String;
+
+public class StringConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str1 = "Hello";
+String str2 = str1.concat(" How are you ?");
+System.out.println(str2);
+	}
+
+}
