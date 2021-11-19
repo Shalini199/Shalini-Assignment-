3cbb67b980f1b5e90ab3bf3dcdb269cf2e83d54b
shalinpa <shalinpa@LIN25008045.corp.capgemini.com>
2021-11-19 08:43:25 +0530

    StringBuffer Assignment

---
+++ StringBuffer/src/StringBuff/StringBuff.java
@@ -0,0 +1,16 @@
+package StringBuff;
+
+public class StringBuff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuffer str = new StringBuffer ("StringBuffer");
+str.append(" is a peer class of String");
+str.append(" that provides much of");
+str.append(" the functionality of strings");
+
+System.out.println(str);
+
+	}
+
+}
